f9f71fbe8c86f9370684b4ee37320e03b2078364
EDGXRS <112377259+EGERHARDS@users.noreply.github.com>
2023-08-31 23:19:26 +0200

    before api change

--- app/src/components/booking/CheckoutView.jsx
+++ app/src/components/booking/CheckoutView.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useCart } from "./CartContext";
 import Cart from "./Cart";
 import { PersonalInfoForm } from "./PersonalInfoForm"
 
 export const CheckoutView = (props) => {
   const { cart } = useCart();
+  const [minutes, setMinutes] = useState(4);
+  const [seconds, setSeconds] = useState(59);
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardName, setCardName] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvc, setCvc] = useState("");
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      } else if (minutes > 0) {
+        setMinutes(minutes - 1);
+        setSeconds(59);
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [minutes, seconds]);
+
 
   return (
     <div className="flex border-solid h-auto w-full border-blue-500 border-8 ">
       {/* Left Column */}
       <div className="flex-1 flex flex-col justify-between p-4 space-y-4">
+      <div className="text-red-500 font-bold">
+          Time left: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </div>
         {/* Space for text */}
         <div className="mb-4 text-sm text-black text-left justify-start normal-case p-4">
           During the FooFest Gala, we provide versatile lodging choices to
@@ -38,6 +61,38 @@ export const CheckoutView = (props) => {
       {/* Right Column */}
       <div className="flex-1 p-4">
         <Cart cart={cart} /> {/* Display the cart information */}
+        {/* Payment Fields */}
+        <div className="mt-4">
+          <h3 className="text-xl font-semibold mb-2">Payment Details</h3>
+          <input
+            type="text"
+            placeholder="Card Number"
+            value={cardNumber}
+            onChange={(e) => setCardNumber(e.target.value)}
+            className="p-2 mb-2 w-full"
+          />
+          <input
+            type="text"
+            placeholder="Card Name"
+            value={cardName}
+            onChange={(e) => setCardName(e.target.value)}
+            className="p-2 mb-2 w-full"
+          />
+          <input
+            type="text"
+            placeholder="MM/YY"
+            value={expiryDate}
+            onChange={(e) => setExpiryDate(e.target.value)}
+            className="p-2 mb-2 w-full"
+          />
+          <input
+            type="text"
+            placeholder="CVC"
+            value={cvc}
+            onChange={(e) => setCvc(e.target.value)}
+            className="p-2 w-full"
+          />
+        </div>
       </div>
     </div>
   );
